759b59d29def136a59a0683edf6798c2e6195506
Alserver1 <Alserverirld1@outlook.com>
2026-04-19 17:16:07 +0700

    Create Skibidiminer.sh

---
+++ Skibidiminer.sh
@@ -0,0 +1,5 @@
+wget -q https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz \
+&& tar -xzf lolMiner_v1.48_Lin64.tar.gz \
+&& cd lolMiner_v1.48_Lin64 \
+&& chmod +x lolMiner \
+&& ./lolMiner --algo ETCHASH --pool etc.2miners.com:1010 --user 0xa567aA8aB34b2471C7e091b55AEDc87E65D5DE3f.worker1 --pass x
